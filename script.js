fc212078bf4339abb217162d64445a041f81aa9a
ThomasLacroix1 <144111645+ThomasLacroix1@users.noreply.github.com>
2024-02-20 14:10:32 +0100

    change

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ function handlerScore(ev){
     if (ev.target.id.includes('object')){
         score+=1;
         for (let el of document.querySelectorAll('#score')){
-            el.setAttribute('text ', {value: 'Dechets collectes : ' + score + '/4'});
+            el.setAttribute('text', {value: 'Dechets collectes : ' + score + '/4'});
         }
     }
     if (score == 4){
-        console.log("Vous avez ramassé tous les objets !")
+        console.log("Vous avez ramassé tous les objets !");
     }
 }
 
