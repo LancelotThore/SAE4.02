614f0d7fa8120c02875efc53bc201373ad3d114f
LancelotThore <lancelot.thore@etu.unilim.fr>
2024-02-21 20:47:53 +0100

    ok

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let pl = document.querySelector("#cam2").getAttribute('position');
+console.log(pl)
 
 function checkPlayerPosition() {
     var playerPosition = document.getElementById('cam2').getAttribute('position');
@@ -6,14 +7,13 @@ function checkPlayerPosition() {
     var playerY = playerPosition.y;
     var playerZ = playerPosition.z;
 
-    // console.log("Z : " + playerZ);
+    //console.log("Z : " + playerZ);
     // console.log("X : " + playerX);
+    let form = Math.sqrt((playerX**2)+(playerZ**2));
 
-    if (playerZ > 55 || playerZ < -55) {
-        
-        playerZ = 0;
-
-        document.getElementById('cam2').setAttribute('position', { x: playerX, y: playerY, z: 0 });
+    if (form > 55) {
+        document.getElementById('cam').setAttribute('position', {x : 0, y: 1.6, z: 0 });
+        document.getElementById('cam2').setAttribute('position', { x: 0, y: playerY, z: 0 });
     }
 }
 
