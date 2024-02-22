a26f58e46bb31feb11c32c67827124c8ac0087a0
thomaslacroix1 <144111645+ThomasLacroix1@users.noreply.github.com>
2024-02-22 14:21:03 +0100

    change

---
+++ jeu/script.js
@@ -0,0 +1,15 @@
+var score = 0;
+
+function handlerScore(ev){
+    if (ev.target.id.includes('object')){
+        score+=1;
+        for (let el of document.querySelectorAll('#score')){
+            el.setAttribute('text', {value: 'Dechets collectes : ' + score + '/5'});
+        }
+    }
+    if (score == 4){
+        console.log("Vous avez ramassé tous les objets !");
+    }
+}
+
+document.addEventListener("click", handlerScore);
