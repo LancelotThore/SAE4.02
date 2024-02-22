65ab2d8165694c8277c1f6a454e66e2b8f876142
LancelotThore <144225580+LancelotThore@users.noreply.github.com>
2024-02-22 14:54:23 +0100

    ajout bordure

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+//score board
 var score = 0;
 
 function handlerScore(ev){
     if (ev.target.id.includes('object')){
-        score+=1;
+        score += 1;
         for (let el of document.querySelectorAll('#score')){
-            el.setAttribute('text', {value: 'Dechets collectes : ' + score + '/5'});
+            el.setAttribute('text', {value: 'Déchets collectés : ' + score + '/5'});
         }
     }
     if (score == 4){
@@ -13,3 +14,25 @@ function handlerScore(ev){
 }
 
 document.addEventListener("click", handlerScore);
+
+// bordure
+function update() {
+    checkPlayerPosition();
+    requestAnimationFrame(update);
+}
+
+function checkPlayerPosition() {
+    var playerPosition = document.getElementById('cam2').getAttribute('position');
+    var playerX = playerPosition.x;
+    var playerY = playerPosition.y;
+    var playerZ = playerPosition.z;
+
+    let form = Math.sqrt((playerX**2)+(playerZ**2));
+
+    if (form > 55) {
+        document.getElementById('cam').setAttribute('position', {x : 0, y: 1.6, z: 0 });
+        document.getElementById('cam2').setAttribute('position', { x: 0, y: playerY, z: 0 });
+    }
+}
+
+requestAnimationFrame(update);
